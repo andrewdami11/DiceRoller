ffde9a88788b2812e330f030d693fe94f41bceae
andrewajube <ajube92@gmail.com>
2019-10-07 11:46:25 +0100

    Deleted CountUp button and edited code reformatting settings for blank lines

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -12,22 +12,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.roll_button)
-        rollButton.setOnClickListener{rollDice()}
-        val countUpButton: Button = findViewById(R.id.count_up_button)
-        countUpButton.setOnClickListener{countUp()}
+        rollButton.setOnClickListener { rollDice() }
     }
 
-    private fun countUp() {
-        val resultText: TextView = findViewById(R.id.result_text)
-        var number = resultText.text.toString().toIntOrNull()
-        resultText.text = number.toString()
-        when (number) {
-            null -> resultText.text = "1"
-            in 1..5 -> {number++; resultText.text = number.toString()}
-        }
-    }
-
-    private fun rollDice(){
+    private fun rollDice() {
         val resultText: TextView = findViewById(R.id.result_text)
         val randomInt: Int = Random.nextInt(6) + 1
         resultText.text = randomInt.toString()
